d5eaa1a293cc4f49045f36cb150f78a5f2d6b809
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2022-03-26 18:09:13 +0800

    js

---
+++ GFL/tempcard.js
@@ -0,0 +1,32 @@
+const tags = {
+
+};
+Object.freeze(tags)
+
+data = [
+    {
+        "questions" : "a",
+        "answers" : "1",
+        "tags" : ["z", "s"]
+    },
+    {
+        "questions" : "b",
+        "answers" : "2",
+        "tags" : ["y"]
+    },
+    {
+        "questions" : "c",
+        "answers" : "3",
+        "tags" : [""]
+    },
+    {
+        "questions" : "d",
+        "answers" : "4",
+        "tags" : ["w"]
+    },
+    {
+        "questions" : "e",
+        "answers" : "5",
+        "tags" : []
+    }
+]
