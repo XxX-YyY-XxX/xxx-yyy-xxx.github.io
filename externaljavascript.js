392a7cfc0a0d6e2dff90234527ea78390cae9d82
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2022-03-27 16:30:26 +0800

    cleanup

--- externaljavascript.js
+++ externaljavascript.js
@@ -9,3 +9,28 @@ function IsSubsetOf(subset, mainset) {
 } else {
     sessionStorage.outputCards = "Sorry, your browser does not support web storage...";
 } */            
+
+//fetch('/GFL/cards.json').then(response => response.json()).then(file => questionCards = file);
+
+/*var oXHR = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+oXHR.onreadystatechange = function() {
+    if (oXHR.readyState === 4 && oXHR.status == 200)
+        questionCards = JSON.parse(this.responseText);
+};
+oXHR.overrideMimeType("application/json");
+oXHR.open("GET", "/GFL/cards.json", true);          // true = ASYNCHRONOUS REQUEST (DESIRABLE), false = SYNCHRONOUS REQUEST.
+oXHR.send(); */
+
+//import questionCards from '/GFL/cards.json';
+//questionCards = JSON.parse(questionCards);
+
+/* const fetchJson = async () => {
+    try {
+        const data = await fetch('/GFL/cards.json');
+        questionCards = await data.json();  
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+fetchJson();*/
