61a4a4eaf967b5ab62deff02a4ed78963e635307
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2022-03-27 16:21:15 +0800

    cleanup

--- externaljavascript.js
+++ externaljavascript.js
@@ -1,3 +1,11 @@
-function IsSubsetOf(subset, mainset){
+function IsSubsetOf(subset, mainset) {
     return subset.every(val => mainset.includes(val));
 }
+
+//replace(/ /g, ' + ');
+
+/*if (typeof(Storage) !== "undefined") {
+    sessionStorage.outputCards = boxes;
+} else {
+    sessionStorage.outputCards = "Sorry, your browser does not support web storage...";
+} */            
