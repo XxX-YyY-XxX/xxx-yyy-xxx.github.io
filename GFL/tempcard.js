cc648a25ec7a518f947c22aef39553614a3fa678
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2022-03-31 10:37:06 +0800

    tag fix

--- GFL/tempcard.js
+++ GFL/tempcard.js
@@ -1,4 +1,4 @@
-const tags = {
+const dataTags = {
     EMPTY : 'Others',
     SF : 'SangvisFerri',
     ACCT : 'AccountManagement',
@@ -36,59 +36,59 @@ const data = [
         questions : `Which SF units are considered dolls/machines/armored/unarmored?`,
         answers : `${image('/GFL/assets/images/SFEnemy.png')}<br>
         Note that Jupiter Cannons count as unarmored machine type.`,
-        tags : [tags.SF]
+        tags : [dataTags.SF]
     },
     {
         questions : `Will there be problems with switching between multiple devices regularly?`,
         answers : `Only if you think that manually logging in every switch is a problem.`,
-        tags : [tags.ACCT]
+        tags : [dataTags.ACCT]
     },
     {
         questions : `Where to go for tech support?`,
         answers : `FB and Twitter is said to be responsive enough. Or you can use ${link('this', 'https://forms.gle/bZNnQeh5sJaD3pim8')} for reporting.`,
-        tags : [tags.TECH, tags["3P"]]
+        tags : [dataTags.TECH, tags["3P"]]
     },
     {
         questions : `Which combat sim is better to focus on?`,
         answers : `Data. Always data.`,
-        tags : [tags.SIMS, tags.RSC]
+        tags : [dataTags.SIMS, dataTags.RSC]
     },
     {
         questions : `How do Neural Upgrades/MODs work?`,
         answers : `${link('Gamepress guide.', 'https://gamepress.gg/girlsfrontline/gfl-explained-neural-upgrade')}<br>
         ${link('Matsuda guide.', 'https://gfl.matsuda.tips/post/modding')}<br>
         ${link('GFC guide.', 'https://www.gflcorner.com/neural')}`,
-        tags : [tags.MOD, tags.PRIME]
+        tags : [dataTags.MOD, dataTags.PRIME]
     },
     {
         questions : `How do I get more Quick Training Contracts?`,
         answers : `Monthlies only.`,
-        tags : [tags.RSC]
+        tags : [dataTags.RSC]
     },
     {
         questions : `I can't access the new chapter even though I already cleared the prerequisite map.`,
         answers : `Beat X-6 Normal of the last chapter again. If it still doesn't appear after that, restart client.`,
-        tags : [tags.MAIN, tags.TECH]
+        tags : [dataTags.MAIN, dataTags.TECH]
     },
     {
         questions : `Which emulators are good for GFL?`,
         answers : `${orderedList('Mumu.', 'Memu.', 'LDPlayer', 'Nox.', 'Bluestacks.')}`,
-        tags : [tags.EMU]
+        tags : [dataTags.EMU]
     },
     {
         questions : `What do I need to consider when using the #2 Chip equipment?`,
         answers : `${link(image('https://big-stupid-jellyfish.github.io/GFMath/pages/images/chips/infographic.png', '', `u/BigStupidJellyfish_'s chip usage flowchart.`), 'https://big-stupid-jellyfish.github.io/GFMath/pages/chips')}`,
-        tags : [tags.TDOLL, tags.EQUIP]
+        tags : [dataTags.TDOLL, dataTags.EQUIP]
     },
     {
         questions : `How can I play GFL if Apple Store doesn't have it?`,
         answers : `Use Android, use emulator, use VPN, use jailbreak.`,
-        tags : [tags.APPLE, tags.ANDROID, tags.EMU]
+        tags : [dataTags.APPLE, dataTags.EMU]
     },
     {
         questions : `When is the anniversary?`,
         answers : `May 8. Basically around the second week of May.`,
-        tags : [tags.ANNIV]
+        tags : [dataTags.ANNIV]
     },
     {
         questions : ``,
