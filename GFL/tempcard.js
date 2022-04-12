bbd50fdc1f6bb9d8960726951b04ce21e5f3f9a9
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2022-04-12 12:14:16 +0800

    Update tempcard.js

--- GFL/tempcard.js
+++ GFL/tempcard.js
@@ -303,10 +303,10 @@ const cardData = Object.freeze([
         tags : [dataTags.BATTLE]
     },
     {
-        questions : `What's the Fire Control Component used for?`,
+        questions : `What's the Fire Control Component (FCC) used for?`,
         answers : `Upgrading 5${star} dolls to 6${star} through modding.`,
         tags : [dataTags.RSC, dataTags.MOD, dataTags.TDOLL]
-    },    
+    },
     {
         questions : `What is Bookshelf of Memories?`,
         answers : `Sidestories for the featured dolls. Rewards Friend Gossips or Unity Skills.`,
