4b662f7e7469a75c39d6a4adcccc2b1ab9d449a2
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2022-05-01 10:55:38 +0800

    test

--- univasset/scripts/includeHTML.js
+++ univasset/scripts/includeHTML.js
@@ -8,37 +8,45 @@ for (const include of document.getElementsByTagName('include')) {
         });                
 }
 
-/* <div w3-include-html="/univasset/styleheader.html"></div> */
+//<div w3-include-html="/univasset/styleheader.html"></div>
 
-/*             function includeHTML() {
-                const z = document.getElementsByTagName("*");
-                for (let i = 0; i < z.length; i++) {
-                    const element = z[i];
-                    const file = element.getAttribute("w3-include-html");
-                    if (file) {
-                        const xhttp = new XMLHttpRequest();
-                        xhttp.onreadystatechange = function() {
-                            if (this.readyState == 4) {
-                                switch (this.status) {
-                                    case 200:
-                                        element.innerHTML = this.responseText;
-                                        break;
-                                    case 404:
-                                        element.innerHTML = file + " not found.";
-                                        break;
-                                    default:
-                                        element.innerHTML = this.status;
-                                        break;
-                                }
-                                element.removeAttribute("w3-include-html");
-                                includeHTML();
-                            }
-                        }
-                        xhttp.open("GET", file, true);
-                        xhttp.send();
-                        return;
+/*
+function includeHTML() {
+    const z = document.getElementsByTagName("*");
+    for (let i = 0; i < z.length; i++) {
+        const element = z[i];
+        const file = element.getAttribute("w3-include-html");
+        if (file) {
+            const xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    switch (this.status) {
+                        case 200:
+                            element.innerHTML = this.responseText;
+                            break;
+                        case 404:
+                            element.innerHTML = file + " not found.";
+                            break;
+                        default:
+                            element.innerHTML = this.status;
+                            break;
                     }
+                    element.removeAttribute("w3-include-html");
+                    includeHTML();
                 }
             }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return;
+        }
+    }
+}
 
-            includeHTML(); */
+includeHTML();
+*/
+
+//<iframe src="/univasset/styleheader.html" onload="this.insertAdjacentHTML('afterend', (this.contentDocument.body||this.contentDocument).innerHTML);this.remove()"></iframe>
+//<object name="styleheader" type="text/html" data="/univasset/styleheader.html"></object>
+//<embed type="text/html" src="/univasset/styleheader.html">
+//<!-- #include virtual="/univasset/styleheader.html" -->
+//{% include /univasset/styleheader.html %}
