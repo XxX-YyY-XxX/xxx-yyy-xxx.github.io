7dbacace7bff5606ff96e318f1f30e1e925eed25
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2022-06-04 10:40:33 +0800

    Update tempcard.js

--- GFL/tempcard.js
+++ GFL/tempcard.js
@@ -150,14 +150,14 @@ const cardData = Object.freeze([
     //#region Topic Primers
     {
         questions : `How do I level my girls?`,
-        answers : `First is grinding them on leveling maps, mainly through ${spoilerSummary('corpse dragging.', 
+        answers : `First is grinding them on leveling maps, mainly through ${spoilerSummary('corpse dragging', 
         `Also called Poor Run or Beggar Run, is a method of leveling dolls (and fairy) using minimal resources. This is done by only supplying a single doll echelon then placing them in a non-supplied echelon composed of dolls you want to level.<br>
         ${link("Ceia's 0-2 drag guide. Docs Edition.", 'https://docs.google.com/document/u/0/d/1PkxJ7ObdGW_cS_qbzAxQ_hoC1SFse3HNYWlnywZfPuo')}<br>
         ${link("Ceia's 0-2 drag guide. Vids Edition.", 'https://youtu.be/vqvPpO1vKqw')}<br>
         ${link('Matsuda guide per leveling map.', 'https://gfl.matsuda.tips/post/leveling_guide')}<br>
         ${link('Gamepress guide.', 'https://gamepress.gg/girlsfrontline/optimizing-leveling-introduction')}<br>
         ${link('GFLCorner guide.', 'https://www.gflcorner.com/efficient-leveling-guide/')}<br>
-        ${link('DMesse guide.', 'http://dmesse.egloos.com/m/3567918')}`)}<br>
+        ${link('DMesse guide.', 'http://dmesse.egloos.com/m/3567918')}`)}.<br>
         Second is using Combat Reports. You can get them in the Forward Basecamp or through the Data Room. You can give them by going to the Dorm>Warehouse>Gift tab or going to your owned doll profile and tapping the + button by the EXP bar. They give 3000 fixed EXP per report, unaffected by dummy link EXP multiplier.`,
         tags : [dataTags.TDOLL, dataTags.LEVEL, dataTags.NEWB, dataTags.PRIME]
     },
@@ -649,7 +649,7 @@ const cardData = Object.freeze([
     {
         questions : `What does oathing a doll do?`,
         answers : `Additional line for oath, higher affection cap, double EXP gain (map EXP, auto-battles, CRs) on MODs, complete repair and resupply (one-time only), stat bonuses for damage/evasion/accuracy (because higher affection cap).`,
-        tags : [dataTags.TDOLL, dataTags.OATH]
+        tags : [dataTags.TDOLL, dataTags.OATH, dataTags.MOD, dataTags.LEVEL, dataTags.LOVE]
     },
     {
         questions : `How do I get more support echelons?`,
