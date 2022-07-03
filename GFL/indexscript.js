ab97c016cc6caae6712bd264c34daf40d06ad7b2
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2022-07-03 14:15:59 +0800

    trial

--- GFL/indexscript.js
+++ GFL/indexscript.js
@@ -11,11 +11,13 @@ const searchParams = new URLSearchParams(location.search);
 //#region Initialize
 document.getElementById('version-number').innerHTML = cardData.length;
 
+//tooltip is only visible when tag is hovered over. 
 toggleableTagsField.innerHTML = Object.values(dTag).sort((a, b) => Compare.string(a, b, 'val')).map(tag => 
-    `<label class="tags tooltip">
-        <input type="checkbox" onclick="toggleTag(this)" value="${tag.val}">${tag.val}
-    </label>
-    <span class="tooltiptext">${tag.desc}</span>`
+    `<label class="tags">
+        <input type="checkbox" onclick="toggleTag(this)" value="${tag.val}">
+        <span class="tooltip">${tag.val}</span>
+        <span class="tooltiptext">${tag.desc}</span>
+    </label>`
 ).join(' ');
 
 document.getElementById('cards-field').innerHTML = (searchParams.has('search') || searchParams.has('tags')) ? searchCards() : randomCards();
