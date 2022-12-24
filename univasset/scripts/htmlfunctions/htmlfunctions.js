0964a3d1c422c5bbccf62879d7fa8e41ae1e455a
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2022-12-24 15:27:34 +0800

    trial

--- univasset/scripts/htmlfunctions/htmlfunctions.js
+++ univasset/scripts/htmlfunctions/htmlfunctions.js
@@ -1,15 +1,6 @@
 const linkStart = ['https://', './assets/images/'];
 const imgAlt = 'Image loading failed.';
 
-export class TextStyle {
-    static STRIKE = 'del';
-    static ITALIC = 'em';
-    static BOLD = 'strong';
-    static CODE = 'code';
-    static QOUTE = 'blockquote';
-    static SUPER = 'sup';
-}
-
 /** @param {string} link */
 export function image(link, caption = "") {
     if (caption) {
@@ -43,14 +34,6 @@ export function table(headerArray, ...arrayOfArrays) {
             ${arrayOfArrays.map(val1 => `<tr>${val1.map(val2 => `<td>${val2}</td>`).join('')}</tr>`).join('')}
         </tbody>    
     </table>`;
-}
-
-/** @param {string} string @param {TextStyle[]} styles First inside, last outside */
-export function altStyle(string, ...styles) {
-    for (const htmlElem of styles) {
-        string = `<${htmlElem}>${string}</${htmlElem}>`;
-    }
-    return string;
 }
 
 export function spoilerSummary(summaryName, details) {
