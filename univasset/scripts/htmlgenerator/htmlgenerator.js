0b1375f6353f3cc790b007f85b031df0f847431d
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2023-05-12 08:53:33 +0800

    Update htmlgenerator.js

--- univasset/scripts/htmlgenerator/htmlgenerator.js
+++ univasset/scripts/htmlgenerator/htmlgenerator.js
@@ -73,165 +73,39 @@ export function gdocDropdown(grouper_elem, ...namelinkpair) {
     grouper_elem.append(select_elem, button_elem, document.createElement("br"), iframe_elem);
 }
 
-/** Creates a table from a given matrix of data.
- * @param {HTMLElement} grouperElem
- * @param {Array[]} tableMatrix First row will be used as header.
- * @param {{sort: boolean|function(HTMLElement), filter: boolean, frzcol: boolean, frzhdr: boolean}} */
-export function table(grouperElem, tableMatrix, {sort = false, filter = false, frzcol = false, frzhdr = false} = {}) {
-    console.error("Use 'tableWithSort' for sorted tables.");
-
-    const headerElems = tableMatrix.shift().map(value => {
+/** Creates a sortable table from a given matrix of data.
+ * @param {HTMLElement} grouper_elem
+ * @param {Array[]} tablematrix First row will be used as header.
+ * @param {(function(HTMLTableCellElement, any): void)[]} mapping
+ * @param {{frzcol: boolean, frzhdr: boolean}} */
+export function table(grouper_elem, tablematrix, mapping, {frzcol = false, frzhdr = false} = {}) {
+    //Almost always a string
+    const [thead_elem, tr_elem] = nestElements("thead", "tr");
+    for (const value of tablematrix.shift()) {
         const th = document.createElement("th");
-        switch (type(value)) {
-            case "string":
-            case "number":
-                th.textContent = value;
-                break;
-            case "array":
-                th.appendChild(brJoin(value));
-                break;
-            case "dom":
-                th.appendChild(value);
-                break;
-            default:
-                console.log(type(value), value);
-                th.textContent = value;
-                break;
-        }
-        return th;
-    });
-    /** @type {HTMLTableSectionElement} */ const theadElem = initializeHTML("thead tr", {append: headerElems});
+        th.textContent = value;
+        tr_elem.appendChild(th);        
+    }
 
-    const tbodyElem = document.createElement('tbody');
-    for (const rows of tableMatrix) {
-        const rowElems = rows.map(value => {
+    const tbodyElem = document.createElement("tbody");
+    for (const rows of tablematrix) {
+        const tr = document.createElement("tr");
+        tr.append(...rows.map((value, index) => {
             const td = document.createElement("td");
-            switch (type(value)) {
-                case "string":
-                case "number":
-                    td.textContent = value;
-                    break;
-                case "array":
-                    td.appendChild(brJoin(value))
-                    break;
-                case "dom":
-                    th.appendChild(value);
-                    break;        
-                default:
-                    console.log(type(value), value);
-                    td.textContent = value;
-            }
+            mapping[index](td, value);
             return td;
-        });
-        tbodyElem.appendChild(initializeHTML("tr", {append: rowElems}));
+        }));
+        tbodyElem.appendChild(tr);
     }
 
-    const tableElem = initializeHTML("table", {append: [theadElem, tbodyElem]});
-    //------------------------------------------------------------------------------------------------------------
-    switch (true) {
-        case sort && filter:
-            alert('Sort and Filter are both activated.');
-            break;
-        case Boolean(sort):
-            //multi sort - 2+ sortings
-            //multiple option sort - multiple keys per cell
+    const table_elem = document.createElement("table");
+    table_elem.append(thead_elem, tbodyElem);
 
-            /* const key = {
-                function: x => sort(x),
-                boolean: x => x
-            }[type(sort)] */
-            /** @type {DOMStringMap} */ const header_data = theadElem.dataset;
-            header_data.onsort = 0;
+    if (frzcol) table_elem.classList.add("freeze_col");
+    if (frzhdr) table_elem.classList.add("freeze_row");
 
-            const samplerow = tableMatrix[0];
-            /** @type {function(HTMLTableRowElement): string|number} */ const leadkey = {
-                string: x => x.firstElementChild.textContent,
-                number: x => Number(x.firstElementChild.textContent),
-                array: x => null,
-                dom: x => null
-            }[type(samplerow[0])];
-
-            /** @param {MouseEvent} event */
-            function sortMethod(event) {
-                /** @type {HTMLTableCellElement} */ const cell = this;
-                const basis_array = {
-                    no() {
-                        const index = cell.dataset.index;
-                        cell.dataset.sort = 'hi';
-    
-                        if (index != header_data.onsort) {
-                            headerElems[header_data.onsort].dataset.sort = 'no';
-                            header_data.onsort = index;
-                        }
-    
-                        return {
-                            string: () => tableMatrix.slice().sort(compare({key: x => x[index]})),
-                            number: () => tableMatrix.slice().sort(compare({key: x => x[index], reverse: true})),
-                            array: () => tableMatrix.slice().sort(compare({key: x => [~x[index].length, x[index]]})),
-                            dom: () => null
-                        }[cell.dataset.type]()
-                    },
-                    hi() {
-                        const index = cell.dataset.index;
-                        cell.dataset.sort = 'lo';
-    
-                        return {
-                            string: () => tableMatrix.slice().sort(compare({key: x => x[index], reverse: true})),
-                            number: () => tableMatrix.slice().sort(compare({key: x => x[index]})),
-                            array: () => tableMatrix.slice().sort(compare({key: x => [x[index].length, x[index]]})),
-                            dom: () => null
-                        }[cell.dataset.type]()
-                    },
-                    lo() {
-                        cell.dataset.sort = 'no';
-    
-                        return tableMatrix;
-                    }
-                }[cell.dataset.sort]().map(row => row[0]);
-
-                const new_sort = Array.from(tbodyElem.children).sort(compare({key: leadkey, array: basis_array}));
-                tbodyElem.replaceChildren(...new_sort);                                                                     
-            }
-
-            for (const [index, headerCell] of Object.entries(headerElems)) {
-                setAttr(headerCell.dataset, {sort: 'no', index: index, type: type(samplerow[index])});
-                headerCell.addEventListener('click', sortMethod, true);
-            }
-            break;
-        case filter:
-            //unfilter
-            //multifilter
-            //change to CSS display: 'none';
-            //filter for header and first column
-            for (const [index, headerCell] of Object.entries(headerElems)) {
-                headerCell.dataset.index = index;
-                if (index) {
-                    headerCell.addEventListener('click', function() {
-                        //const index = this.dataset.index;
-                        //printDataRows(tablerows.filter(x => x[index]));
-                    });
-                } else {
-                    headerCell.addEventListener('click', function() {
-                        //printDataRows(tablerows);
-                    });
-                }
-            }
-
-            for (const [index, trElem] of Object.entries(tbodyElem.children)) {
-                trElem.dataset.index = index;
-                trElem.firstElementChild.addEventListener('click', function() {
-
-                });
-            }
-            break;
-        //default: Simple af table.
-    }
-
-    if (frzcol) tableElem.classList.add('freeze_col');
-    if (frzhdr) tableElem.classList.add('freeze_row');
-    //------------------------------------------------------------------------------------------------------------
-    grouperElem.classList.add('func_table');
-    grouperElem.appendChild(tableElem);
+    grouper_elem.classList.add("func_table");
+    grouper_elem.appendChild(table_elem);
 }
 
 /** Creates a sortable table from a given matrix of data.
@@ -323,7 +197,8 @@ export function tableSort(grouper_elem, tablematrix, mapping, {frzcol = false, f
  * @param {HTMLElement} grouper_elem
  * @param {string} date Mon dy, year hr:mn (UTC|GMT)±offs
  * @param {string} eventURL URL of the banner image.
- * @param {{onEnd: function(): void, interval: number}} onEnd Event listener for when timer reaches 0.
+ * @param {{onEnd: function(): void, interval: number}}
+ * @param onEnd Event listener for when timer reaches 0.
  * @param interval Time it takes to update the timer, in milliseconds. Default 1000. */
 export function timer(grouper_elem, date, eventURL = '', {onEnd = null, interval = 1000} = {}) {
     const classtext = "func_timer";
