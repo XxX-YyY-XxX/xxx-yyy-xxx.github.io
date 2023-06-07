9158d2e324948cc243d01d346c7e1fde6dc7cb41
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2023-06-08 01:10:14 +0800

    clean

--- univasset/scripts/include.js
+++ univasset/scripts/include.js
@@ -2,31 +2,36 @@
     src: url of file whose content will replace this element
         <include src="" a="12" s="34"></include>
 
-    key: parameter name whose value will replace this element
+    key: parameter qualifiedName whose value will replace this element
         <include key="s"></include>
         => 34
 
-    key-???: change own name to ???, current value is parameter name, change value to value of parameter
-        <include src="" key-z="a"></include>
+    param-???: change own qualifiedName to ???, current value is parameter qualifiedName, change value to value of parameter
+        <include src="" param-z="a"></include>
         => <include src="" z="12"></include>
 
     ChildNode.replaceWith() does not work with open tags.
     object, array, boolean ???
 */
 
-for (const INCLUDE of Array.from(document.querySelectorAll("include[src]"))) await includeDocument(INCLUDE);
+for (const INCLUDE of Array.from(document.querySelectorAll("include[src]"))) await includeDocument(INCLUDE, location.pathname);
 
-/** @param {HTMLElement} include_elem */
-async function includeDocument(include_elem) {
+/**
+ * @param {HTMLElement} include_elem
+ * @param {string} file_name Name of file where include element is taken. Used for error handling. */
+async function includeDocument(include_elem, file_name) {
     const SOURCE = include_elem.getAttribute("src");
     const INCLUDE_DOC = await fetch(SOURCE)
         .then(response => {
             if (response.ok) return response.text();
-            return Promise.reject(`File missing: ${SOURCE}`)})
+            return Promise.reject(`Missing ${SOURCE} in ${file_name}`)})
         .then(html => html.replace(/<!--(?!>)[\S\s]*?-->/g, ""))
         .then(cleantext => new DOMParser().parseFromString(cleantext, "text/html"))
         .catch(error => {console.error(error); return null});
-    if (INCLUDE_DOC === null) return;
+    if (INCLUDE_DOC === null) {
+        include_elem.remove();
+        return;
+    }
 
     const PARAM = new Map(Array.from(include_elem.attributes).map(({name, value}) => [name, value]));
 
@@ -35,15 +40,15 @@ async function includeDocument(include_elem) {
 
     for (const INCLUDE of Array.from(INCLUDE_DOC.querySelectorAll("include"))) {
         for (const {name, value} of Array.from(INCLUDE.attributes)) {
-            if (name.startsWith("key-")) {
-                INCLUDE.setAttribute(name.replace("key-", ""), PARAM.get(value));
+            if (name.startsWith("param-")) {
+                INCLUDE.setAttribute(name.replace("param-", ""), PARAM.get(value));
                 INCLUDE.removeAttribute(name);
             }
         }
     }
     
     for (const INCLUDE of Array.from(INCLUDE_DOC.querySelectorAll("include[src]")))
-        await includeDocument(INCLUDE);
+        await includeDocument(INCLUDE, SOURCE);
 
     for (const INCLUDE of Array.from(INCLUDE_DOC.querySelectorAll("include")))
         console.log("Wild include element found:", INCLUDE, "from", SOURCE);
