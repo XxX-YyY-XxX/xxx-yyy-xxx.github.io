8fc3f7a213e657a5844c174a29535c12cfea4e46
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2023-07-05 11:28:40 +0800

    Update index.js

--- univasset/scripts/basefunctions/index.js
+++ univasset/scripts/basefunctions/index.js
@@ -1,4 +1,4 @@
-export * from "./typehint.d.ts";
+//export * from "./typehint.d.ts";      uncomment later when github ts mime bs is fixed
 
 export * from "./pseudobuiltin.js";
 import "./prototype.js";
