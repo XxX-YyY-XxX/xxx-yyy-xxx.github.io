e81e7b356ce2f72944abeac0cbf9991308241f35
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2023-08-29 01:23:51 +0800

    test

--- GFL/index.js
+++ GFL/index.js
@@ -6,8 +6,8 @@ timer(
     "https://www.iopwiki.com/images/9/9d/Longitudinal_Strain_Login_Wallpaper.jpg",
     {onEnd: timer.bind(
         null, document.getElementById("event"),
-        "",
-        ""
+        "Sep 18, 2023 23:59 UTC-0800",
+        "./assets/images/timer/ujmrjucqfsib1.webp"
     )}
 );
 
@@ -28,8 +28,8 @@ timer(
     "./assets/images/coalition/AlinaMosasaurRider.webp", 
     {onEnd: timer.bind(
         null, document.getElementById("coalition"),
-        "",
-        ""
+        "Sep 18, 2023 23:59 UTC-0800",
+        "./assets/images/coalition/Scarecrow.webp"
     )}
 );
 
@@ -39,13 +39,16 @@ timer(
     "./assets/images/timer/SkinBannerNow.webp",
     {onEnd: timer.bind(
         null, document.getElementById('skin-banner'),
-        "",
-        "./assets/images/timer/SkinBannerNext.webp"
+        "Sep 25, 2023 23:59 UTC-0800",
+        "./assets/images/timer/a7bhcugaxlib1.webp"
     )}
 );
 
-//timer(document.getElementById('mini-event'), "",
-//    "");
+timer(
+    document.getElementById('mini-event'),
+    "Sep 18, 2023 23:59 UTC-0800",
+    "./assets/images/timer/9btufbjh4tjb1.webp"
+);
 
 //timer(document.getElementById('extra'), "",
 //    "");
--- PNC/units.js
+++ PNC/units.js
@@ -160,16 +160,15 @@ class Units {
         this.#regen = BASE.regen;
 
         const POT = stat_object.potential;
-        //this.#pothash = POT.hash;
         //this.#potpdef = POT.pdef;
         //this.#potodef = POT.odef;
         //this.#potppen = POT.ppen;
         //this.#potopen = POT.open;
         this.#potregen = POT.regen;
 
-        const STAT = "hash"
+        const STAT = "pdef"
         console.log(
-            this.name,
+            this.name, "PDef:",
             Math.floor(BASE[STAT] * 0.61) === POT[STAT],
             POT[STAT] / BASE[STAT]
         )
@@ -186,18 +185,30 @@ class Units {
         this.#intistats = stat_object.intimacy;
 
         const TD_NAME = document.createElement("td");
-        const IMAGE = document.createElement("img");
-        IMAGE.addEventListener("load", () => {
+        //const IMAGE = document.createElement("img");
+        //IMAGE.addEventListener("load", () => {
+        //    this.#hasarma = true;
+        //    setattr(IMAGE, {loading: "lazy", alt: `${this.name} arma.`});
+        //    const SPAN = setattr(document.createElement("span"), {append: [this.name, IMAGE], classList: {add: ["arma"]}});
+        //    TD_NAME.appendChild(SPAN);
+        //});
+        //IMAGE.addEventListener("error", () => {
+        //    this.#hasarma = false;
+        //    TD_NAME.textContent = this.name;
+        //});
+        //IMAGE.src = `./assets/images/arma/${this.name.replace(" ", "")}.png`;
+
+        try {
             this.#hasarma = true;
-            setattr(IMAGE, {loading: "lazy", alt: `${this.name} arma.`});
+            const IMAGE = setattr(document.createElement("img"), {loading: "lazy", alt: `${this.name} arma.`, src: `./assets/images/arma/${this.name.replace(" ", "")}.png`});
             const SPAN = setattr(document.createElement("span"), {append: [this.name, IMAGE], classList: {add: ["arma"]}});
             TD_NAME.appendChild(SPAN);
-        });
-        IMAGE.addEventListener("error", () => {
+
+        } catch (error) {
+            console.log(error);
             this.#hasarma = false;
             TD_NAME.textContent = this.name;
-        });
-        IMAGE.src = `./assets/images/arma/${this.name.replace(" ", "")}.png`;
+        }
 
         const TD_HP = document.createElement("td");
         const TD_ATK = document.createElement("td");
