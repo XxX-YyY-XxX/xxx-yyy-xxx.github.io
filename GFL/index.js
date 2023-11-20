9ead4f571c955055db00c82db5bd7897d2c7a6c0
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2023-11-20 22:39:05 +0800

    clean

--- GFL/index.js
+++ GFL/index.js
@@ -8,12 +8,12 @@ timer(
 
 timer(
     document.getElementById("battlepass"),
-    "Nov 19, 2023 23:59 UTC-0800",
-    "./assets/images/timer/c6rh2we7vhtb1.webp",
+    "Dec 24, 2023 23:59 UTC-0800",
+    "./assets/images/timer/1zqlm8vfpn0c1.webp",
     {onEnd: timer.bind(
         null, document.getElementById("battlepass"),
-        "Dec 24, 2023 23:59 UTC-0800",
-        "./assets/images/timer/1zqlm8vfpn0c1.webp"
+        "",
+        "./assets/images/timer/.webp"
     )}
 );
 
@@ -34,7 +34,7 @@ timer(
     "./assets/images/timer/sjlg3j4ed4wb1.webp",
     {onEnd: timer.bind(
         null, document.getElementById('skin-banner'),
-        "",
+        "Nov 20, 2023 23:59 UTC-0800",
         ""
     )}
 );
