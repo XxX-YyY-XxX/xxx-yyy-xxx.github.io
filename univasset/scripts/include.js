fac60360e7ab23fdbfbab360e18630979e97cc84
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2023-11-28 12:34:12 +0800

    clean

--- univasset/scripts/include.js
+++ univasset/scripts/include.js
@@ -2,24 +2,31 @@
     src: url of file whose content will replace this element
         <include src="" a="12" s="34"></include>
 
-    key: parameter qualifiedName whose value will replace this element
+    key: parent parameter whose value will replace this element
         <include key="s"></include>
         => 34
+    
+    ------------------------------------------------------------------------------------------------------------
+    
+    attr-???: change first child element's ??? attribute value to parameter value, must have a child element
+        <include attr-href="s"><a></a></include> or <include attr-href="s"><a href="sample"></a></include>
+        => <a href="34"></a>
 
-    param-???: change own qualifiedName to ???, current value is parameter qualifiedName, change value to value of parameter
+        param-???: change own qualifiedName to ???, current value is parent parameter, change value to value of parameter
         <include src="" param-z="a"></include>
         => <include src="" z="12"></include>
-
+    
     ChildNode.replaceWith() does not work with open tags.
-    object, array, boolean ???
+    array, boolean ???
 */
 
-for (const INCLUDE of Array.from(document.querySelectorAll("include[src]"))) await includeDocument(INCLUDE, location.pathname);
+for (const INCLUDE of Array.from(document.querySelectorAll("include[src]"))) await includeDocument(INCLUDE, location.pathname, 0);
 
 /**
  * @param {HTMLElement} include_elem
- * @param {string} file_name Name of file where include element is taken. Used for error handling. */
-async function includeDocument(include_elem, file_name) {
+ * @param {string} file_name Name of file where include element is taken. Used for error handling.
+ * @param {number} depth File call depth. Could be used to prevent looping. */
+async function includeDocument(include_elem, file_name, depth) {
     const SOURCE = include_elem.getAttribute("src") ?? "";
     const INCLUDE_DOC = await fetch(SOURCE)
         .then(response => response.ok ? response.text() : Promise.reject(`Missing ${SOURCE} in ${file_name}`))
@@ -40,23 +47,49 @@ async function includeDocument(include_elem, file_name) {
         else                        defaultValue(INCLUDE);
     }
 
-    //attr-??? where ??? is attribute of first child, value is parameter name
+    //------------------------------------------------------------------------------------------------------------
 
+    // https://stackoverflow.com/questions/41623353/queryselector-get-elements-where-attribute-starts-with-xxx
+    // document.evaluate()
+
+    //untested
+    for (const INCLUDE of Array.from(INCLUDE_DOC.querySelectorAll("include"))) {
+        const CHILD = INCLUDE.firstElementChild;
+        if (!CHILD) continue;
+
+        for (const {name, value} of Array.from(INCLUDE.attributes)) {
+            if (name.startsWith("attr-")) {
+                const VALUE = PARAM.get(value);
+                if (VALUE !== undefined)    CHILD.setAttribute(name.replace("attr-", ""), VALUE);
+                else                        console.warn("Parameter", value, "called by", name, "not found");
+            }
+        }
+
+        INCLUDE.replaceWith(CHILD);
+    }
+
+    //untested
     for (const INCLUDE of Array.from(INCLUDE_DOC.querySelectorAll("include"))) {
         for (const {name, value} of Array.from(INCLUDE.attributes)) {
             if (name.startsWith("param-")) {
-                //what if param key not found?
-                INCLUDE.setAttribute(name.replace("param-", ""), PARAM.get(value));
+                const VALUE = PARAM.get(value);
+                if (VALUE !== undefined)    INCLUDE.setAttribute(name.replace("param-", ""), VALUE);
+                else                        console.warn("Parameter", value, "called by", name, "not found");
                 INCLUDE.removeAttribute(name);
             }
         }
     }
     
+    //untested
+    //what if looping to itself/alternate looping/pass looping
     for (const INCLUDE of Array.from(INCLUDE_DOC.querySelectorAll("include[src]")))
-        await includeDocument(INCLUDE, SOURCE);
+        await includeDocument(INCLUDE, SOURCE, depth + 1);
 
+    //untested
     for (const INCLUDE of Array.from(INCLUDE_DOC.querySelectorAll("include")))
-        console.warn("Wild include element found:", INCLUDE, "from", SOURCE);
+        console.warn("Wild include element found:", INCLUDE.outerHTML, "from", SOURCE);
+
+    //------------------------------------------------------------------------------------------------------------
     
     include_elem.replaceWith(...INCLUDE_DOC.body.childNodes);
 }
