e8bed2405246b28b8fc48dae30c09679fe378c55
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2023-12-26 10:06:58 +0800

    update

--- GFL/index.js
+++ GFL/index.js
@@ -2,17 +2,17 @@ import {timer} from "../univasset/scripts/htmlgenerator/htmlgenerator.js";
 
 timer(
    document.getElementById("event"),
-   "Dec 25, 2023 23:59 UTC-0800",
-   "https://iopwiki.com/images/7/71/Event_Logo_One_Coin_Short.png"
+   "Jan 22, 2024 23:59 UTC-0800",
+   "https://iopwiki.com/images/3/35/Login_Slow_Shock.jpg"
 );
 
 timer(
     document.getElementById("battlepass"),
-    "Dec 24, 2023 23:59 UTC-0800",
-    "./assets/images/timer/1zqlm8vfpn0c1.webp",
+    "Jan 28, 2024 23:59 UTC-0800",
+    "./assets/images/timer/8fawb6hfle8c1.webp",
     {onEnd: timer.bind(
         null, document.getElementById("battlepass"),
-        "Jan 28, 2024 23:59 UTC-0800",
+        "",
         "./assets/images/timer/.webp"
     )}
 );
@@ -23,19 +23,19 @@ timer(
     "./assets/images/coalition/17DestroyerDashingReindeer.webp", 
     {onEnd: timer.bind(
         null, document.getElementById("coalition"),
-        "",
-        "./assets/images/coalition/.webp"
+        "Jan 22, 2024 23:59 UTC-0800",
+        "./assets/images/coalition/.webp"   //Tareus
     )}
 );
 
 timer(
     document.getElementById('skin-banner'),
-    "Dec 18, 2023 23:59 UTC-0800",
-    "./assets/images/timer/2kcrg4upyo1c1.webp",
+    "Jan 15, 2024 23:59 UTC-0800",
+    "https://iopwiki.com/images/c/c8/Dreams_By_The_Fireplace_Login_Wallpaper.png",
     {onEnd: timer.bind(
         null, document.getElementById('skin-banner'),
-        "Jan 15, 2024 23:59 UTC-0800",
-        "https://iopwiki.com/images/c/c8/Dreams_By_The_Fireplace_Login_Wallpaper.png"
+        "",
+        ""
     )}
 );
 
