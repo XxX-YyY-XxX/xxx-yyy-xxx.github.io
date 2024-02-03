06e9e6f17249f726be2e850e4ed2cd3822209d9b
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2024-02-03 13:41:20 +0800

    clean

--- GFL/index.js
+++ GFL/index.js
@@ -1,30 +1,30 @@
 import {timer} from "../univasset/scripts/htmlgenerator/htmlgenerator.js";
 
-timer(
-   document.getElementById("event"),
-   "Jan 22, 2024 23:59 UTC-0800",
-   "https://iopwiki.com/images/3/35/Login_Slow_Shock.jpg"
-);
+// timer(
+//    document.getElementById("event"),
+//    "",
+//    ""
+// );
 
 timer(
     document.getElementById("battlepass"),
-    "Jan 28, 2024 23:59 UTC-0800",
-    "./assets/images/timer/8fawb6hfle8c1.webp",
+    "Mar 04, 2024 23:59 UTC-0800",
+    "./assets/images/timer/vbkwzn9f4bec1.webp",
     {onEnd: timer.bind(
         null, document.getElementById("battlepass"),
-        "Mar 04, 2024 23:59 UTC-0800",
-        "./assets/images/timer/vbkwzn9f4bec1.webp"
+        "",
+        "./assets/images/timer/.webp"
     )}
 );
 
 timer(
     document.getElementById("coalition"),
-    "Jan 22, 2024 23:59 UTC-0800",
-    "./assets/images/coalition/22Tareus.webp", 
+    "Feb 05, 2024 24:00 UTC-0800",
+    "./assets/images/coalition/06Architect.webp", 
     {onEnd: timer.bind(
         null, document.getElementById("coalition"),
-        "Feb 05, 2024 23:59 UTC-0800",
-        "./assets/images/coalition/06Architect.webp"
+        "Mar 05, 2024 24:00 UTC-0800",
+        "./assets/images/coalition/23ScarecrowDemonicAbyss.webp"
     )}
 );
 
