9e8bc5336a9afedf038def532ca5e58c5e49edcf
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2024-03-30 17:56:54 +0800

    fix

--- univasset/scripts/html/index.js
+++ univasset/scripts/html/index.js
@@ -110,7 +110,7 @@ export function details(summary, content) {
 
 export function fragment(...nodes) {
     const O = setattr(new DocumentFragment(), {append: nodes});
-    // console.log(O)
+    console.log(O?.toString())
     return O;
 }
 
