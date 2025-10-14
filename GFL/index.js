2477152d5cc5d9c583401c770140aff708b0913a
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2025-10-15 02:04:45 +0800

    timer

--- GFL/index.js
+++ GFL/index.js
@@ -1,33 +1,33 @@
 import {timer} from "../univasset/scripts/htmlgenerator/htmlgenerator.js";
 
-//timer(
-//   document.getElementById("event"),
-//   "Sep 23, 2025 00:00 UTC-0800",
-//   "./assets/images/timer/gp6qlw3cbqmf1.webp"
-//);
+timer(
+   document.getElementById("event"),
+   "Oct 28, 2025 00:00 UTC-0800",
+   "./assets/images/timer/G3N3IvubEAAFkwr.jpg"
+);
 
 timer(
     document.getElementById("battlepass"),
     "Oct 20, 2025 00:00 UTC-0800",
     "./assets/images/timer/0x6c9jqo39of1.webp",
-    //{onEnd: timer.bind(null,
-    //    document.getElementById("battlepass"),
-    //    "Oct 20, 2025 00:00 UTC-0800",
-    //    "./assets/images/timer/0x6c9jqo39of1.webp",
-    //)}
+    {onEnd: timer.bind(null,
+        document.getElementById("battlepass"),
+        "Nov 24, 2025 00:00 UTC-0800",
+        "./assets/images/timer/.webp", //stg
+    )}
 );
 
 timer(
     document.getElementById("coalition"),
-    "Oct 14, 2025 00:00 UTC-0800",
-    "./assets/images/coalition/02Executioner.webp"
+    "Nov 11, 2025 00:00 UTC-0800",
+    "./assets/images/coalition/26DreamerMoonlitSentinel.webp"
 );
 
-timer(
-    document.getElementById('skin-banner'),
-    "Sep 30, 2025 00:00 UTC-0800",
-    "https://iopwiki.com/images/thumb/7/78/MK3A1_costume3_D.png/600px-MK3A1_costume3_D.png"
-);
+//timer(
+//    document.getElementById('skin-banner'),
+//    "Sep 30, 2025 00:00 UTC-0800",
+//    "https://iopwiki.com/images/thumb/7/78/MK3A1_costume3_D.png/600px-MK3A1_costume3_D.png"
+//);
 
 //timer(
 //   document.getElementById('mini-event'),
