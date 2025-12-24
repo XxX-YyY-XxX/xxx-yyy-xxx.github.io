bbf937bd3e3115b31069c34fe5853b20d9eb8619
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2025-12-24 15:41:12 +0800

    timer

--- GFL/index.js
+++ GFL/index.js
@@ -1,21 +1,26 @@
 import {timer} from "../univasset/scripts/htmlgenerator/htmlgenerator.js";
 
-//timer(
-//   document.getElementById("event"),
-//   "Dec 02, 2025 00:00 UTC-0800",
-//   "./assets/images/timer/dz94kjpln93f1.webp"
-//);
+timer(
+   document.getElementById("event"),
+   "Jan 06, 2025 00:00 UTC-0800",
+   "./assets/images/timer/rank.png"
+);
 
 timer(
     document.getElementById("battlepass"),
     "Dec 29, 2025 00:00 UTC-0800",
     "./assets/images/timer/sgsw10r3hx1g1.webp",
+    {onEnd: timer.bind(null,
+        document.getElementById("battlepass"),
+        "Feb 02, 2025 00:00 UTC-0800",
+        "./assets/images/timer/8cln2wrn4o8g1.webp",
+    )}
 );
 
 timer(
     document.getElementById("coalition"),
-    "Dec 09, 2025 00:00 UTC-0800",
-    "./assets/images/coalition/16IntruderSorceressOfTheEnd.webp"
+    "Jan 06, 2025 00:00 UTC-0800",
+    "./assets/images/coalition/17DestroyerDashingReindeer.webp"
 );
 
 timer(
@@ -24,8 +29,8 @@ timer(
     "./assets/images/timer/ibwz2rbn8i4g1.webp"
 );
 
-timer(
-   document.getElementById('mini-event'),
-   "Dec 23, 2025 00:00 UTC-0800",
-   "./assets/images/timer/78bh0schsq3g1.webp"
-);
+//timer(
+//   document.getElementById('mini-event'),
+//   "Dec 23, 2025 00:00 UTC-0800",
+//   "./assets/images/timer/78bh0schsq3g1.webp"
+//);
