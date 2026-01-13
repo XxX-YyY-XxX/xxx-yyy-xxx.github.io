c64b0a38f4f90c27bd3c0d0a28318b3680fe6a93
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2026-01-13 20:18:22 +0800

    timer

--- GFL/index.js
+++ GFL/index.js
@@ -1,10 +1,10 @@
 import {timer} from "../univasset/scripts/htmlgenerator/htmlgenerator.js";
 
-//timer(
-//   document.getElementById("event"),
-//   "Jan 06, 2026 00:00 UTC-0800",
-//   "./assets/images/timer/rank.png"
-//);
+timer(
+   document.getElementById("event"),
+   "Feb 03, 2026 00:00 UTC-0800",
+   "./assets/images/timer/4uqku68vk3dg1.webp"
+);
 
 timer(
     document.getElementById("battlepass"),
@@ -18,11 +18,11 @@ timer(
     "./assets/images/coalition/12Agent(P).webp"
 );
 
-//timer(
-//    document.getElementById('skin-banner'),
-//    "Dec 30, 2025 00:00 UTC-0800",
-//    "./assets/images/timer/ibwz2rbn8i4g1.webp"
-//);
+timer(
+    document.getElementById('skin-banner'),
+    "Feb 10, 2026 00:00 UTC-0800",
+    "https://iopwiki.com/images/thumb/a/ad/Howa_Type_64_costume3_D.png/600px-Howa_Type_64_costume3_D.png"
+);
 
 //timer(
 //   document.getElementById('mini-event'),
