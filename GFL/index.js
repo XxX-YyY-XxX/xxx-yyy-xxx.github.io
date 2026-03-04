83dd9d800e54f1aacd792b006198ba1cbffe9ce7
XxX-YyY-XxX <98796368+XxX-YyY-XxX@users.noreply.github.com>
2026-03-04 20:11:51 +0800

    timer

--- GFL/index.js
+++ GFL/index.js
@@ -1,21 +1,26 @@
 import {timer} from "../univasset/scripts/htmlgenerator/htmlgenerator.js";
 
-timer(
-   document.getElementById("event"),
-   "Feb 24, 2026 00:00 UTC-0800",
-   "./assets/images/timer/y7gmkdv461hd1.webp"
-);
+//timer(
+//   document.getElementById("event"),
+//   "Feb 24, 2026 00:00 UTC-0800",
+//   "./assets/images/timer/y7gmkdv461hd1.webp"
+//);
 
 timer(
     document.getElementById("battlepass"),
     "Mar 09, 2026 00:00 UTC-0800",
-    "./assets/images/timer/z65gm4ia3tfg1.webp"
+    "./assets/images/timer/z65gm4ia3tfg1.webp",
+    {onEnd: timer(null,
+        document.getElementById("battlepass"),
+        "Apr 13, 2026 00:00 UTC-0800",
+        "./assets/images/timer/t5qp3pfp3rmg1.webp"
+    )}
 );
 
 timer(
     document.getElementById("coalition"),
-    "Mar 03, 2026 00:00 UTC-0800",
-    "./assets/images/coalition/23ScarecrowDemonicAbyss.webp"
+    "Mar 31, 2026 00:00 UTC-0800",
+    "./assets/images/coalition/18HunterNightfallMoth.webp"
 );
 
 //timer(
